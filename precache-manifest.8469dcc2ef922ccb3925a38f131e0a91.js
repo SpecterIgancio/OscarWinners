ebbdb9c7736710210e6d83fc111764290d85652f
SpecterIgancio <57646723+SpecterIgancio@users.noreply.github.com>
2020-02-17 21:30:45 -0300

    Updates

---
+++ precache-manifest.8469dcc2ef922ccb3925a38f131e0a91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8806cca50adbf4899a2b1f1e76be0f",
+    "url": "/OscarWinners/index.html"
+  },
+  {
+    "revision": "7a8f428aa2464073eb7b",
+    "url": "/OscarWinners/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4edd4105924a5f9d0e48",
+    "url": "/OscarWinners/static/css/main.90154ee9.chunk.css"
+  },
+  {
+    "revision": "7a8f428aa2464073eb7b",
+    "url": "/OscarWinners/static/js/2.5a39e62a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/OscarWinners/static/js/2.5a39e62a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4edd4105924a5f9d0e48",
+    "url": "/OscarWinners/static/js/main.6eba4247.chunk.js"
+  },
+  {
+    "revision": "4bf1fe41b3eb57e498df",
+    "url": "/OscarWinners/static/js/runtime-main.8f9f1112.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/OscarWinners/static/media/SEGA.12a3db8b.woff"
+  }
+]);
